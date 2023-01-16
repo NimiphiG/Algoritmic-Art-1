37d61ba9072aa928e25bf46bdea539c570e31f17
NimiphiG <115160805+NimiphiG@users.noreply.github.com>
2023-01-16 18:32:26 +0000

    comented

--- js/task1.js
+++ js/task1.js
@@ -1,25 +1,20 @@
 
-// set global variables
-let x ;
-let y ;
-let theta ;
-let maxAngle;
-let a;
-let clicked = 1;
-let growth = 1
-
 //how far each frame moves through the waves
-let inc = 0.03; 
-let offset = 0; 
-
-let length = 10; // sets number of elipses drawn by the for loop
-let fr = 10; // sets the initial fps
+let offset = 0;
+let inc = 0.03;
 
 
+// sets number of elipses drawn by the for loop
+let length = 10;
+
+// sets the initial fps
+let fr = 10;
+
+// sets the starting point for the mouse functions
+let clicked = 1;
+let growth = 1;
 
 function setup() {
-
-    maxAngle = (0.05) * TAU; //sets a limit of the sin wave
 
     createCanvas(windowWidth, windowHeight);
 
@@ -27,59 +22,69 @@ function setup() {
     centerX = windowWidth / 2;
     centerY = windowHeight / 2;
 
-    window.addEventListener('mousedown', mouseDown) //lower frame rate when mouse is held down
-    function mouseDown() {
-        fr = 0.5;
-        
-
-        //increase the value of clicked untill 6 then decrese untill 1
-        clicked += growth;
-        if (clicked > 5) {
-            growth = -1
-        };
-        if ((clicked == 1) && (growth == -1)) {
-            growth = 1
-        }
-    }
-    window.addEventListener('mouseup', mouseUp)
-    function mouseUp() {
-        fr = 10 //if the mouse is relesed return to 10fps
-    }
 }
 
 function draw() {
 
-    background(0); // clear each frame before drawing the next
     frameRate(fr) // sets the frame rate based on mouse listeners 
+    background(0); // clear each frame before drawing the next
 
     //moves the sine wave up by inc for the next frame next frame
     offset += inc;
     theta = offset;
 
-    while (theta < maxAngle + offset) { //makes sure each frame only runs through the for loop once
+    for (let i = length; i > 0; i--) {
 
-        for (let i = length; i > 0; i--) {
+        // draws an elipse with a random x position that scales with cosine, a size that scales with sine and styled acording to loop iteration 
+        a = cos(theta);
+        x = (random(-centerX, centerX) * a);
 
-            // draws an elipse with a random x position that scales with cosine, a size that scales with sine and styled acording to loop iteration 
-            a = cos(theta)
-            x = (random(-400, 400) * a) + (random(-400, 400) * a);
-            stroke(225 - (255 / length * i), 60, 50, 225);
-            strokeWeight(10 * i);
-            fill(200, 100, 0, 60);
-            ellipse((x) + centerX, centerY, sin(theta) * 400 * (i / 3), (sin(theta)) * 300 * (i / 3));
+        width = 130 * sin(theta) * i;
+        height = 100 * (sin(theta)) * i;
 
-            //draws an elipse in the center with style and size based on mouse clicked
-            strokeWeight(5 * clicked );
-            stroke(199.5, 60, 50);
-            fill(0, 0, 0, clicked * clicked * (255 / 80));
-            ellipse(centerX, centerY, 25 * clicked * clicked, 25 * clicked * clicked);
-        }
+        stroke(225 - (255 / length * i), 60, 50, 225);
+        strokeWeight(10 * i);
+        fill(200, 100, 0, 60);
 
-        theta += 1; //ends the while loop for this frame
+        ellipse(x + centerX, centerY, width, height);
+
 
     }
+    //draws an elipse in the center with style and size based on mouse clicked
+    stroke(255, 60, 50);
+    fill(0, 0, 0, clicked * (35));
+    strokeWeight(5 * clicked);
 
+    width = 25 * clicked * clicked;
+    height = 25 * clicked * clicked;
+
+    ellipse(centerX, centerY, width, height);
 }
+
+window.addEventListener('mousedown', mouseDown)
+window.addEventListener('mouseup', mouseUp)
+
+function mouseDown() {
+    fr = 0.5;//lower frame rate when mouse is held down
+
+    //increase the value of clicked untill 6 then decrese untill 1
+    clicked += growth;
+    if (clicked > 5) {
+        growth = -1
+    };
+    if ((clicked == 1) ) {
+        growth = 1
+    }
+}
+
+function mouseUp() {
+    fr = 10; //if the mouse is relesed return to 10fps
+}
+
+
+
+
+
 
 
 //random movement gets bigger when mouse is further from the center
